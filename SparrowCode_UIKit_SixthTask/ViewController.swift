e979c10105b92eb98d3ee25163d9f04c0ebcd3b1
Edmond <edpodlegaev@yandex.ru>
2024-04-18 20:18:53 +0300

    Complete Task

--- SparrowCode_UIKit_SixthTask/ViewController.swift
+++ SparrowCode_UIKit_SixthTask/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var square: UIView = {
+        let view = UIView()
+        view.bounds.size = .init(width: 100, height: 100)
+        view.layer.cornerRadius = 10
+        view.backgroundColor = .systemBlue
+        return view
+    }()
+    
+    lazy var dynamicAnimator = UIDynamicAnimator(referenceView: view)
+    lazy var snapBehavior = UISnapBehavior(item: square, snapTo: view.center)
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(square)
+        square.center = view.center
+        dynamicAnimator.addBehavior(snapBehavior)
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchCoordinates = touches.first?.location(in: view) else { return }
+        snapBehavior.snapPoint = touchCoordinates
+    }
 }
-
